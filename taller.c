1e1f202c66bf4fc265f6aaf63a2afff840a8c473
Yurem Morejon <sebastian.morejon@udla.edu.ec>
2026-04-18 18:26:19 -0500

    Ingreso de notas con ciclos for y validación do-while entre 0 y 10

--- taller.c
+++ taller.c
@@ -10,5 +10,27 @@ printf("Sistema de Calificaciones \n");
 printf("Estudiantes: %d\n", A);
 printf("Asignaturas: %d\n", B);
 
+for (i = 0; i < A; i++)
+{
+    printf("Estudiante %d \n", i+1);
+    for (j = 0; j < B; j++){
+       do{
+        printf("Asignatura %d: ", j+1);
+        scanf("%f", &notas[i][j]);
+        if(notas[i][j]<0||notas[i][j]>10)
+        {
+            printf("Nota invalida, ingrese nota entre 0 y 10\n");
+   
+        }
+        
+       }while(notas[i][j]<0||notas[i][j]>10);
+       {
+    
+       }
+       
+    }
+    
+}
+
 return 0;
 }
