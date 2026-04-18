8a299932bbd6c03158eec74698ad0abb6f4b66c9
Yurem Morejon <sebastian.morejon@udla.edu.ec>
2026-04-17 23:55:00 -0500

    Definicion de variables A,B, matriz notas y encabezado

---
+++ taller.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int A=5;
+    int B=3;
+    float notas[A][B];
+    int i,j;
+
+printf("Sistema de Calificaciones \n");
+printf("Estudiantes: %d\n", A);
+printf("Asignaturas: %d\n", B);
+
+return 0;
+}
